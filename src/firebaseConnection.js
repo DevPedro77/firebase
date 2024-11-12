1a273f2d10d2dc768825a35fec9dad96dfc812e2
Pedro Castro <pcastro9876@gmail.com>
2024-11-12 16:18:42 -0300

    configurando o firebase

---
+++ src/firebaseConnection.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3ynGhFxh8PG2WquCEp4rrTKk74GztE6k",
+  authDomain: "current-825a2.firebaseapp.com",
+  projectId: "current-825a2",
+  storageBucket: "current-825a2.firebasestorage.app",
+  messagingSenderId: "138730210485",
+  appId: "1:138730210485:web:08edd6efa8d58a4e4eeafc",
+  measurementId: "G-1GM4R205EF"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db}
